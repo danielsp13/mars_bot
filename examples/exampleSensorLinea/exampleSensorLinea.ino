4844c45004d368c073d707e6cf72e48f739ad870
Daniel Pérez Ruiz <danielperezruiz.10@gmail.com>
2023-06-23 20:34:43 +0200

    examples de testeo, #7, #11

---
+++ examples/exampleSensorLinea/exampleSensorLinea.ino
@@ -0,0 +1,48 @@
+/**
+ * @file exampleSensorLinea.ino
+ * @authors Martin Anaya Quesada, Daniel Perez Ruiz
+ * @brief Fichero de pruebas del controlador de SensorLinea
+ * @version 0.1.0
+ */
+
+#include "sensores/linea/sensorLinea.h"
+
+// Definicion de pines
+#define PIN_SENSORLINEA_IZDA 8
+#define PIN_SENSORLINEA_DCHA 9
+
+// Definicion de controladores de sensor
+SensorLinea sensorIzda = SensorLinea("SLINEA_IZDA",PIN_SENSORLINEA_IZDA);
+SensorLinea sensorDcha = SensorLinea("SLINEA_DCHA",PIN_SENSORLINEA_DCHA);
+
+const uint16_t DELAY = 1500;
+const String SEPARATOR = "==========================================================";
+
+/**
+ * @brief Realiza pruebas sobre el sensor de linea especificado como argumento
+ * @param s : Sensor de linea al que realizar pruebas
+ */
+void testSensor(const SensorLinea & s){
+    //1. Comprobar superficie
+    s.lineaNegra();
+    //2. Imprimir informacion
+    s.printInfo();
+}
+
+// =================================================================================
+
+void setup(){
+    Serial.begin(9600);
+}
+
+void loop(){
+    Serial.println(SEPARATOR);
+    // -- Llamada a las funciones de prueba
+    testSensor(sensorIzda);
+    testSensor(sensorDcha);
+    Serial.println(SEPARATOR);
+    Serial.println();
+    
+    // -- Realizar delay de espera
+    delay(DELAY);
+}
